23c253ba1624d3207f15f51bdefdc19d22947863
Alexander Demin <alexander@demin.ws>
2025-11-12 16:26:31 +0000

    fix ci

--- i8080_test.c
+++ i8080_test.c
@@ -39,17 +39,21 @@
 #include "i8080.h"
 #include "i8080_hal.h"
 
-void load_file(const char* name, unsigned char* load_to) {
-    FILE* f = fopen(name, "r+b");
+void load_file(const char *name, unsigned char *load_to)
+{
+    FILE *f = fopen(name, "r+b");
     int sz;
-    if (!f) {
+    if (!f)
+    {
         fprintf(stderr, "Unable to open file \"%s\"\n", name);
         exit(1);
     }
     sz = 0;
-    while (!feof(f)) {
+    while (!feof(f))
+    {
         int const read = fread((void *)load_to, 1, 1024, f);
-        if (read == 0) break;
+        if (read == 0)
+            break;
         sz += read;
         load_to += read;
     }
@@ -57,33 +61,40 @@ void load_file(const char* name, unsigned char* load_to) {
     printf("File \"%s\" loaded, size %d\n", name, sz);
 }
 
-void execute_test(const char* filename, int success_check) {
-    unsigned char* mem = i8080_hal_memory();
+void execute_test(const char *filename, int success_check)
+{
+    unsigned char *mem = i8080_hal_memory();
     int success = 0;
 
     memset(mem, 0, 0x10000);
     load_file(filename, mem + 0x100);
 
-    mem[5] = 0xC9;  // Inject RET at 0x0005 to handle "CALL 5".
+    mem[5] = 0xC9; // Inject RET at 0x0005 to handle "CALL 5".
     i8080_init();
     i8080_jump(0x100);
-    while (1) {
+    while (1)
+    {
         int const pc = i8080_pc();
-        if (mem[pc] == 0x76) {
+        if (mem[pc] == 0x76)
+        {
             printf("HLT at %04X\n", pc);
             exit(1);
         }
-        if (pc == 0x0005) {
-            if (i8080_regs_c() == 9) {
+        if (pc == 0x0005)
+        {
+            if (i8080_regs_c() == 9)
+            {
                 int i;
                 for (i = i8080_regs_de(); mem[i] != '$'; i += 1)
                     putchar(mem[i]);
                 success = 1;
             }
-            if (i8080_regs_c() == 2) putchar((char)i8080_regs_e());
+            if (i8080_regs_c() == 2)
+                putchar((char)i8080_regs_e());
         }
         i8080_instruction();
-        if (i8080_pc() == 0) {
+        if (i8080_pc() == 0)
+        {
             printf("\nJump to 0000 from %04X\n", pc);
             if (success_check && !success)
                 exit(1);
@@ -100,7 +111,8 @@ int main()
     execute_test("assets/8080PRE.COM", 1);
     execute_test("assets/8080EX1.COM", 0);
     const time_t end = time(NULL);
-    printf("\n@ executed in %ld second(s)\n", end - start);
+    const int diff = end - start;
+    printf("\n@ executed in %d second(s)\n", diff);
 
     return 0;
 }
